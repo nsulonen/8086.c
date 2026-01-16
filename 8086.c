b2120551b8c06a74348d7c6bd5252f7a979cda54
nsulonen <niko.sulonen@proton.me>
2026-01-16 16:41:39 +0200

    ...

--- 8086.c
+++ 8086.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
   if (argc < 2 || argc > 2) {
-    printf("Usage %s <input>\n", argv[0]);
+    printf("Usage %s <filepath>\n", argv[0]);
     exit(1);
   }
 
@@ -14,27 +14,16 @@ int main(int argc, char *argv[]) {
   }
 
   fseek(input, 0, SEEK_END);
-  // long file_size = ftell(input);
+  int file_size = ftell(input);
   rewind(input);
 
-  // int *buffer = malloc(file_size);
-  // if (!buffer) {
-  //   perror("Memory allocation failed!");
-  //   fclose(input);
-  //   exit(1);
-  // }
-
-  int bytes[2];
-  
-  int count = fread(bytes, 1, 1, input);
+  unsigned char bytes[2];
+  int count = fread(bytes, 1, file_size, input);
   fclose(input);
 
   for (int i = 0; i < count; i++) {
     printf("Byte %d: %b\n", i + 1, bytes[i]);
   }
-  // printf("Number of objects read: %d\n", count);
-  // printf("%b\n", *buffer);
-  // free(bytes);
 
   printf("Program complete!\n");
   exit(0);
